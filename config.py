b62b4d0ed7fe562fb23cbb08a611d39e0287c643
Flash11552 <sfrovli7@gmail.com>
2024-12-07 19:09:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/frozenoffical/frozenvcmusic",
+    "https://github.com/Flash11552/effectbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
